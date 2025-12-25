104955386303530b3b909a2f76bb01696bd6c2d7
Khoa Le <khoale@mbp14.local>
2025-12-24 23:07:58 -0600

    Fix: Add missing timezone import

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
